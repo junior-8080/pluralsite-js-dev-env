962b55f3c42377fd94266feb3774309af3fb6efb
abdul mukhsin <aabdulmukhsin@gmail.com>
2020-07-06 20:18:50 +0000

    http-call-data-mock-api

--- src/index.js
+++ src/index.js
@@ -1,16 +1,29 @@
 import './index.css'
-import {getUsers} from './api/userApi'
+import {getUsers, deleteUsers} from './api/userApi'
 
 getUsers().then(result => {
   let userBody = "";
   result.forEach(user => {
     userBody+=`<tr>
       <td><a href="#" data-id="${user.id}" class="deleteUser">Delete</a></td>
-      <td>${user.firstname}</td>
-      <td>${user.lastname}></td>
+      <td>${user.firstName}</td>
+      <td>${user.lastName}</td>
       <td>${user.email}</td>
       </tr>
     `
   });
   global.document.getElementById('users').innerHTML = userBody;
-})
+
+  const deleteLinks = document.getElementsByClassName('deleteUser');
+
+
+  Array.from(deleteLinks,link => {
+    link.onclick = function(event){
+      const element = event.target;
+      event.preventDefault();
+      deleteUsers(element.attributes["data-id"].value);
+      const row = element.parentNode.parentNode;
+      row.parentNode.removeChild(row);
+    }
+  });
+});
